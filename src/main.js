e7162f7304417f700c59bb738f40cd42bce7f5af
alpachm <alexpacheco407@gmail.com>
2022-12-28 18:12:55 -0500

    portafolio_5

---
+++ src/main.js
@@ -0,0 +1,6 @@
+let icon = document.getElementById('icon_menu');
+let menu = document.getElementById('menu_text')
+
+icon.addEventListener('click', function(){
+    menu.classList.toggle('show_menu')
+})
