d6c878f5e0cf9e1c2dd69743e747b5c5f3c25da6
alpachm <alexpacheco407@gmail.com>
2023-01-03 10:02:18 -0500

    menu

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 let icon = document.getElementById('icon_menu');
 let menu = document.getElementById('menu_text')
 
-icon.addEventListener('click', function(){
+function clickear(){
     menu.classList.toggle('show_menu')
-})
+}
+
+icon.addEventListener('click', clickear)
+
+const menuA = document.querySelectorAll('#menu_text a');
+
+for(i of menuA){
+    i.addEventListener('click', clickear)
+}
+
+console.log(menu);
